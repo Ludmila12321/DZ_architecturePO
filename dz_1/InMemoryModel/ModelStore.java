749acaab4e9a2d9d30a0e5afb2d47108d73b755c
Lyudmila <lyudmila.kostornova@mail.ru>
2023-08-20 23:11:48 +0300

    Первое задание

--- dz_1/InMemoryModel/ModelStore.java
+++ dz_1/InMemoryModel/ModelStore.java
@@ -1,44 +1,46 @@
 package InMemoryModel;
+
 import java.util.ArrayList;
 import java.util.List;
 import ModelElements.Camera;
 import ModelElements.Flash;
 import ModelElements.PoligonalModel;
 import ModelElements.Scene;
-public class ModelStore implements IModelChanger {
+
+public class ModelStore implements IModelChanger{
     public List<PoligonalModel> Models;
     public List<Scene> Scenes;
     public List<Flash> Flashes;
     public List<Camera> Cameras;
-    private List<IModelChangerObserver> changerObservers; 
+    private List<IModelChangedObserver> changeObservers;
 
-    public ModelStore(List<IModelChangerObserver> changerObservers, List<PoligonalModel> models, List<Scene> scene, List<Flash> Flashes, List<Camera> Cameras) throws Exception {
-       
-        this.changerObservers = changerObservers;
-        Models = new ArrayList<>(null);
-        Scenes = new ArrayList<>(null);
-        Flashes = new ArrayList<>(null) ;
-        Cameras = new ArrayList<>(null);
+    public ModelStore(List<IModelChangedObserver> changeObservers) throws Exception {
+        this.changeObservers = changeObservers;
+        this.Models = new ArrayList<>();
+        this.Scenes = new ArrayList<>();
+        this.Flashes = new ArrayList<>();
+        this.Cameras = new ArrayList<>();
 
-        Models.add(new PoligonalModel(null));
+        Models.add(new PoligonalModel( null));
         Flashes.add(new Flash());
         Cameras.add(new Camera());
         Scenes.add(new Scene(0, Models, Flashes, Cameras));
 
-        }
-
-    public void NotifyChange(IModelChanger sender) {
     }
 
-    public Scene getScena(int id) {
+    public Scene GetScene(int id){
         for (int i = 0; i < Scenes.size(); i++) {
-
-            if(Scenes.get(i).Id == id) {
+            if (Scenes.get(i).id == id){
                 return Scenes.get(i);
-            }   
+            }
+
         }
         return null;
+    }
 
-    }    
-    
+
+    @Override
+    public void NotifyCange(IModelChanger sender) {
+
+    }
 }
